5826a6ba1e78c38ba8a4dab9058fd859870b6450
Lisa <mkoliju@gmail.com>
2022-09-27 10:09:05 -0700

    Fix margins

--- form.js
+++ form.js
@@ -5,14 +5,7 @@ const phone = document.getElementById("user_number");
 const firstPswd = document.getElementById("user_password");
 const confirmPswd = document.getElementById("confirm_pswd");
 
+const form = document.getElementById("spaceForms")
 const createBTN = document.getElementById("createBTN");
 
 
-createBTN.addEventListener("click", (event) => {
-    if (firstName.validity.typeMismatch){
-        firstName.setCustomValidity("Please enter your name.");
-        firstName.reportValidity();
-    } else {
-        firstName.setCustomValidity("");
-    }
-});
